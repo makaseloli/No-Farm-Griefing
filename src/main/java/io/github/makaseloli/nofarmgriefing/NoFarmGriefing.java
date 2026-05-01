c10232a58cffcb10c07ee8be4775f42e1832e2d2
makaseloli <114403771+makaseloli@users.noreply.github.com>
2026-05-01 11:48:31 +0900

    Port to Fabric 26.1

--- src/main/java/io/github/makaseloli/nofarmgriefing/NoFarmGriefing.java
+++ src/main/java/io/github/makaseloli/nofarmgriefing/NoFarmGriefing.java
@@ -1,20 +1,9 @@
 package io.github.makaseloli.nofarmgriefing;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.level.BlockEvent;
+import net.fabricmc.api.ModInitializer;
 
-@Mod(NoFarmGriefing.MOD_ID)
-public final class NoFarmGriefing {
-    public static final String MOD_ID = "nofarmgriefing";
-
-    public NoFarmGriefing(IEventBus modEventBus, ModContainer modContainer) {
-        NeoForge.EVENT_BUS.addListener(NoFarmGriefing::onFarmlandTrample);
-    }
-
-    private static void onFarmlandTrample(BlockEvent.FarmlandTrampleEvent event) {
-        event.setCanceled(true);
+public final class NoFarmGriefing implements ModInitializer {
+    @Override
+    public void onInitialize() {
     }
 }
